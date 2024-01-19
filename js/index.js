b9b5a6c7ffe11a5ca2edde00673221075b038313
IsaacDJohnson <isaac.designws@gmail.com>
2024-01-19 10:41:43 -0600

    completed

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@
   greetProgrammer();
   => "Hello, programmer!"
 */
-function greetProgrammer() {
-  console.log("Hello, programmer!");
-}
+// function greetProgrammer() {
+//   console.log("Hello, programmer!");
+// }
 
 /*
   You should be able to call this function with one argument and see its output in the terminal:
   greet("Naureen");
   => "Hello, Naureen!"
 */
-function greet(name) {
-  console.log(`Hello, ${name}!`);
-}
+// function greet(name) {
+//   console.log(`Hello, ${name}!`);
+// }
 
 /*
   You should be able to call this function with no arguments and see its output in the terminal:
@@ -25,9 +25,9 @@ function greet(name) {
   greetWithDefault("Sunny");
   => "Hello, Sunny!"
 */
-function greetWithDefault(name = "programmer") {
-  console.log(`Hello, ${name}!`);
-}
+// function greetWithDefault(name = "programmer") {
+//   console.log(`Hello, ${name}!`);
+// }
 
 /*
   You should be able to call this function with two arguments and get back its return value:
@@ -35,9 +35,9 @@ function greetWithDefault(name = "programmer") {
   console.log(sum);
   => 3
 */
-function add(num1, num2) {
-  return num1 + num2;
-}
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
 
 /*
   You should be able to call this function with two arguments and get back its return value:
